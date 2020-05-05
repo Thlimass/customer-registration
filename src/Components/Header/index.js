98422b4bf6671fedd4d93e87c81f7ad1da687081
Thlimass <thlima.silva.souza@gmail.com>
2020-05-05 15:26:11 -0300

    criado icone do header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-
+import { MDBIcon } from "mdbreact";
+import 'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Header = () => {
 
         return(
             <nav className="navbar navbar-dark bg-primary">
                 <a className="navbar-brand" href="#">
-                    <img src="" width="30" height="30"
-                         className="d-inline-block align-top" alt="" />
-                        Cadastro de Cliente
+                    <MDBIcon far icon="user-circle" size="lg" className="mr-3" />
+                    Cadastro de Cliente
                 </a>
             </nav>
         );
