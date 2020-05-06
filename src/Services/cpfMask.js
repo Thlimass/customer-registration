93f730416c72dbedbf32697f8a749755fa95174f
Thlimass <thlima.silva.souza@gmail.com>
2020-05-06 13:32:48 -0300

    método criado para mascara do input CPF

---
+++ src/Services/cpfMask.js
@@ -0,0 +1,16 @@
+export const cpfMask = value =>{
+    return value
+        .replace(/\D/g, '')// substitui qualquer caracter que nao seja numero por nada
+        .replace(/(\d{3})(\d)/, '$1.$2')// captura 2 grupos de numero o primeiro de 3 e o segundo de 1, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de numero
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')// captura 2 numeros seguidos de um traço e não deixa ser digitado mais nada
+}
+
+//     Linha 1: exportando nossa função. Essa função é bem simples: ela simplesmente recebe um valor e o retorna.
+//     Linha 3: substitui qualquer caractere que não seja número por nada.
+//     Linha 4: captura 2 grupos de numero o primeiro de 3 números (\d{3})e o segundo de 1 número (\d).
+//     Após capturar o primeiro grupo de numero $1. ele adiciona um ponto antes do segundo grupo de numero $2.
+//     Linha 6: faz o mesmo replace que as linhas 4 e 5, mas adiciona um - no lugar do ponto.
+//     Linha 7: captura 2 números seguidos de um traço, ou seja, os três últimos caracteres do CPF e não deixa ser digitado mais nada.
+//     Pronto, nossa mascara já pode ser usada. O código do nosso App.js precisa ser atualizado.
